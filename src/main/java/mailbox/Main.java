1cc4cdcd6123c7a6f699c311c433d1a65c6f03ab
Cosmos <cosmos.moderation@gmail.com>
2019-06-24 19:00:58 -0700

    finalized logging initialization

--- src/main/java/mailbox/Main.java
+++ src/main/java/mailbox/Main.java
@@ -1,10 +1,19 @@
 package mailbox;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.util.logging.ExceptionLogger;
+import org.javacord.core.util.logging.LoggerUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
- *
  * @author Cosmos
  */
 public class Main {
@@ -12,14 +21,41 @@ public class Main {
     // Used to allow calls from other classes.
     public static org.javacord.api.DiscordApi api;
 
+    public static final Logger logger = LoggerUtil.getLogger(Main.class);
+
     /*
     Main method that handles the bot initial connection from runtime arguments (Bot token)
      */
     public static void main(String args[]) {
 
+
+        logger.info("Mailbox - Designed by Cosmos open source under GPL 3.0 license. "
+                + "Special thanks to everyone on the Javacord Discord server for creating such a great library");
+
+        // Used for when Log4j isn't in use. Not recommended
+        //FallbackLoggerConfiguration.setDebug(true);
+
         // Use bot token in command arguments to run bot.
         api = new DiscordApiBuilder().setToken(args[0]).login().join();
 
+    }
+
+    /*
+Handles the logger initialization so it can be called from other classes to output to console
+ */
+    private static void initializeLogging() throws IOException {
+        System.setProperty("java.util.logging.manager", LogManager.class.getName());
+        String log4jConfigurationFileProperty = System.getProperty("log4j.configurationFile");
+        if (log4jConfigurationFileProperty != null) {
+            Path log4jConfigurationFile = Paths.get(log4jConfigurationFileProperty);
+            if (!Files.exists(log4jConfigurationFile)) {
+                try (InputStream fallbackLog4j2ConfigStream = Main.class.getResourceAsStream("/log4j2.xml")) {
+                    Files.copy(fallbackLog4j2ConfigStream, log4jConfigurationFile);
+                }
+            }
+        }
+
+        Thread.setDefaultUncaughtExceptionHandler(ExceptionLogger.getUncaughtExceptionHandler());
     }
 
 }
