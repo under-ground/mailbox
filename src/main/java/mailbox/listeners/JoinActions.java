c72e6a51e37dfe28e635835cec3d374728ff2240
Cosmos <cosmos.moderation@gmail.com>
2019-06-24 19:08:27 -0700

    JoinActions class added which will be used to perform tasks once the bot joins the server.

---
+++ src/main/java/mailbox/listeners/JoinActions.java
@@ -0,0 +1,16 @@
+package mailbox.listeners;
+
+
+import org.javacord.api.event.server.ServerJoinEvent;
+import org.javacord.api.listener.server.ServerJoinListener;
+
+
+public class JoinActions  implements ServerJoinListener {
+
+    @Override
+    public void onServerJoin(ServerJoinEvent event) {
+
+        // TODO: Handle activity once the bot joins the server
+
+    }
+}
