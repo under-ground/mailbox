3577d82f9b15372d1138e9235934a0dafdfc9da9
Cosmos <cosmos.moderation@gmail.com>
2019-06-24 19:04:52 -0700

    added MessageListener class which will serve as primary class to handle bot commands

---
+++ src/main/java/mailbox/listeners/MessageActions.java
@@ -0,0 +1,17 @@
+package mailbox.listeners;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
+
+/**
+ *
+ * @author Cosmos
+ */
+public class MessageActions implements MessageCreateListener {
+
+    @Override
+    public void onMessageCreate(MessageCreateEvent event) {
+        // TODO Message system commands & administrative commands
+    }
+
+}
