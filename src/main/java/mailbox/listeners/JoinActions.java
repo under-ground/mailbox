cba516c59bf4909b077020c158de8b6015746a12
Cosmos <cosmos.moderation@gmail.com>
2019-06-24 19:35:07 -0700

    changed code to allow automatic creation of data directory if it does not currently exist

--- src/main/java/mailbox/listeners/JoinActions.java
+++ src/main/java/mailbox/listeners/JoinActions.java
@@ -7,17 +7,37 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.javacord.api.event.server.ServerJoinEvent;
 import org.javacord.api.listener.server.ServerJoinListener;
 
+import java.io.File;
 import java.io.IOException;
 
 
 public class JoinActions implements ServerJoinListener {
 
+    /**
+     * Handles events on bot server join
+     * @param event
+     */
     @Override
     public void onServerJoin(ServerJoinEvent event) {
 
+        File directory = new File("./data/");
+
+
+        // Verifies directory exists and creates it if not
+        if (!directory.exists()){
+            directory.mkdir();
+        }
+
         try {
+
             GuildUtil.configureGuild(event.getServer().getId());
-        } catch (IOException | ConfigurationException e) {
+
+        } catch (ConfigurationException e) {
+
+            Main.logger.info(e.getMessage());
+
+        } catch (IOException e) {
+
             Main.logger.info(e.getMessage());
 
         }
