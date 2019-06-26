b2e42890ad8efa49dc5bdc012c7994d9a0f30f1b
Cosmos <cosmos.moderation@gmail.com>
2019-06-25 18:14:42 -0700

    added generic server info command for administrative use

--- src/main/java/mailbox/listeners/MessageActions.java
+++ src/main/java/mailbox/listeners/MessageActions.java
@@ -2,10 +2,13 @@ package mailbox.listeners;
 
 import mailbox.GuildUtil;
 import org.apache.commons.configuration.ConfigurationException;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
+import java.awt.*;
 import java.nio.file.FileAlreadyExistsException;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
@@ -46,6 +49,27 @@ public class MessageActions implements MessageCreateListener {
                         event.getChannel().sendMessage("The configuration file for this server has not been set up or is corrupted");
                     }
 
+                    break;
+
+                case "serverinfo":
+                    // Builds embed
+                    if (event.getMessageAuthor().isServerAdmin()) {
+                        Date serverCreationDate = Date.from(event.getServer().get().getCreationTimestamp());
+                        EmbedBuilder serverinfo = new EmbedBuilder()
+                                .setThumbnail(event.getServer().get().getIcon().get())
+                                .setTitle("**Server Information:**").setColor(Color.white)
+                                .addField("**Server Name:**", event.getServer().get().getName(), true)
+                                .addField("**Server ID:**", event.getServer().get().getIdAsString(), true)
+                                .addField("**Member Count:**", Integer.toString(event.getServer().get().getMemberCount()), true)
+                                .addField("**ServerCreation Date:**", serverCreationDate.toString(), true)
+                                .addField("**Server Owner:**", event.getServer().get().getOwner().toString(), true)
+                                .addField("**Server Region:**", event.getServer().get().getRegion().toString(), true)
+                                .addField("**Verification Level:**", event.getServer().get().getVerificationLevel().toString(), true)
+                                .addField("**Text Channels:**", Integer.toString(event.getServer().get().getTextChannels().size()), true)
+                                .addField("**Voice Channels:**", Integer.toString(event.getServer().get().getVoiceChannels().size()), true);
+                        // Send the embed. It logs every exception, besides missing permissions (you are not allowed to send newMessage in the channel)
+                        event.getChannel().sendMessage(serverinfo);
+                    }
                     break;
 
                 case "reset":
