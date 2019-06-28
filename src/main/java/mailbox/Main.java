098295606acb8f6f11c0acbc3820240bb99de415
Cosmos <cosmos.moderation@gmail.com>
2019-06-27 18:00:40 -0700

    added line to register the ReactionActions class listener

--- src/main/java/mailbox/Main.java
+++ src/main/java/mailbox/Main.java
@@ -2,6 +2,7 @@ package mailbox;
 
 import mailbox.listeners.JoinActions;
 import mailbox.listeners.MessageActions;
+import mailbox.listeners.ReactionActions;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.javacord.api.DiscordApiBuilder;
@@ -42,6 +43,7 @@ public class Main {
         // Registering listener classes
         api.addServerJoinListener(new JoinActions());
         api.addMessageCreateListener(new MessageActions());
+        api.addReactionAddListener(new ReactionActions());
 
     }
 
