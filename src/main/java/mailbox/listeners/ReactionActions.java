23a51f3b1582cdf6fd10f7366de16770418fd7bc
Cosmos <cosmos.moderation@gmail.com>
2019-06-27 17:51:21 -0700

    Used to monitor if someone clicks the ❌ reaction applied to all messages in the inbox.      * This allows for easier monitoring for staff members to see when a message has been handled.

---
+++ src/main/java/mailbox/listeners/ReactionActions.java
@@ -0,0 +1,28 @@
+package mailbox.listeners;
+
+import mailbox.GuildUtil;
+import org.javacord.api.event.message.reaction.ReactionAddEvent;
+import org.javacord.api.listener.message.reaction.ReactionAddListener;
+
+
+
+public class ReactionActions implements ReactionAddListener {
+
+
+    /**
+     * Used to monitor if someone clicks the ❌ reaction applied to all messages in the inbox.
+     * This allows for easier monitoring for staff members to see when a message has been handled.
+     * @param event
+     */
+    @Override
+    public void onReactionAdd(ReactionAddEvent event) {
+        if (event.getChannel().getId() == Long.parseLong(GuildUtil.getInboxChannelId(event.getServer().get().getId()))) {
+            if (event.getEmoji().asUnicodeEmoji().orElseThrow(() -> new AssertionError("This should not be triggered by custom emoji")) == "❌" ) {
+                event.requestMessage().thenAccept(message -> {
+                    message.removeAllReactions();
+                    message.addReaction("✅");
+                });
+            }
+        }
+    }
+}
