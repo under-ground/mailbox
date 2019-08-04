c23e600819b0a124ab5c8f513595e80dba529a10
Cosmos <cosmos.moderation@gmail.com>
2019-08-03 17:24:11 -0700

    collapsed try/catch blocks and organized imports

--- src/main/java/mailbox/listeners/MessageActions.java
+++ src/main/java/mailbox/listeners/MessageActions.java
@@ -11,16 +11,19 @@ import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
-
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.Date;
+import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
-import java.util.List;
 
 import static mailbox.GuildUtil.addMessageChannels;
 
@@ -36,11 +39,11 @@ public class MessageActions implements MessageCreateListener {
     public void onMessageCreate(MessageCreateEvent event) {
 
         if (!event.getServer().isPresent()) return;
-        Server server = event.getServer().get();
+        Server guild = event.getServer().get();
 
         try {
 
-            if (event.getChannel().getIdAsString() == null ? GuildUtil.getInboxChannelId(server.getId(), event.getApi()) == null : event.getChannel().getIdAsString().equals(GuildUtil.getMessageChannelId(server.getId(), event.getApi()))) {
+            if (event.getChannel().getIdAsString() == null ? GuildUtil.getInboxChannelId(guild.getId(), event.getApi()) == null : event.getChannel().getIdAsString().equals(GuildUtil.getMessageChannelId(guild.getId(), event.getApi()))) {
 
                 event.getApi().getThreadPool().getExecutorService().submit(() -> {
                     try {
@@ -60,7 +63,7 @@ public class MessageActions implements MessageCreateListener {
 
                             EmbedBuilder privateMessage = new EmbedBuilder()
                                     .setThumbnail(event.getMessageAuthor().getAvatar())
-                                    .setTitle("**Your message to the " + server.getName() + " staff was successful**").setColor(Color.green);
+                                    .setTitle("**Your message to the " + guild.getName() + " staff was successful**").setColor(Color.green);
                             if (!newUserMessage.isEmpty()) {
                                 // If there is text, it will be added to the embed
                                 privateMessage.addField("Message", newUserMessage, true);
@@ -68,7 +71,14 @@ public class MessageActions implements MessageCreateListener {
                             if (!MessageAttachments.isEmpty()) {
                                 // Adds an image to the embed, if there is one
                                 if (MessageAttachments.get(0).isImage()) {
-                                    privateMessage.setImage(MessageAttachments.get(0).downloadAsByteArray().join());
+                                    BufferedImage image = null;
+                                    try {
+                                        image = ImageIO.read(new File(MessageAttachments.get(0).getUrl().getPath()));
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                    privateMessage.setThumbnail(image, "gif");
                                 }
                             }
                             event.getApi().getUserById(event.getMessageAuthor().getId()).thenAcceptAsync(user -> {
@@ -76,7 +86,7 @@ public class MessageActions implements MessageCreateListener {
 
                                     user.sendMessage(privateMessage);
                                 } catch (NoSuchElementException | CompletionException ex) {
-                                    event.getApi().getChannelById(GuildUtil.getInboxChannelId(server.getId(), event.getApi())).get().asTextChannel().get().sendMessage("❌ " + event.getMessageAuthor().asUser().get().getNicknameMentionTag()
+                                    event.getApi().getChannelById(GuildUtil.getInboxChannelId(guild.getId(), event.getApi())).get().asTextChannel().get().sendMessage("❌ " + event.getMessageAuthor().asUser().get().getNicknameMentionTag()
                                             + " sent us a message, but they are unable to receive direct messages.");
 
                                     BotUtil.messageSelfDestruct(event, event.getMessageAuthor().asUser().get().getNicknameMentionTag()
@@ -97,21 +107,27 @@ public class MessageActions implements MessageCreateListener {
                             if (!MessageAttachments.isEmpty()) {
                                 // Adds an image to the embed, if there is one
                                 if (MessageAttachments.get(0).isImage()) {
-                                    userMessage.setImage(MessageAttachments.get(0).downloadAsByteArray().join());
-                                }
+                                    BufferedImage image = null;
+                                    try {
+                                        image = (BufferedImage) ImageIO.getImageReaders(MessageAttachments.get(0).getUrl().getContent());
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    userMessage.setImage(image, "gif");
+                            }
                             }
                             // Puts the sending user's ID at the footer of the embed
                             userMessage.setFooter("ID: " + event.getMessageAuthor().getIdAsString());
 
 
                             // Sends the newMessage in the Receiving Channel
-                            event.getApi().getChannelById(GuildUtil.getInboxChannelId(server.getId(), event.getApi())).get().asTextChannel().get().sendMessage(userMessage).thenAcceptAsync(message -> {
+                            event.getApi().getChannelById(GuildUtil.getInboxChannelId(guild.getId(), event.getApi())).get().asTextChannel().get().sendMessage(userMessage).thenAcceptAsync(message -> {
                                 // Deletes the newMessage
                                 event.getMessage().delete();
                                 // Adds ❌ reaction to allow staff to see if a message has not been responded to yet
                                 message.addReaction("❌");
                                 // Sends the sending user's ID in the Receiving Channel for ease-of-access copy-paste
-                                event.getApi().getChannelById(GuildUtil.getInboxChannelId(server.getId(), event.getApi())).get().asTextChannel().get().sendMessage(event.getMessageAuthor().getIdAsString());
+                                event.getApi().getChannelById(GuildUtil.getInboxChannelId(guild.getId(), event.getApi())).get().asTextChannel().get().sendMessage(event.getMessageAuthor().getIdAsString());
                             });
 
 
@@ -122,13 +138,13 @@ public class MessageActions implements MessageCreateListener {
                 });
             }
 
-            if (event.getChannel().getIdAsString() == null ? GuildUtil.getInboxChannelId(server.getId(), event.getApi()) == null : event.getChannel().getIdAsString().equals(GuildUtil.getInboxChannelId(server.getId(), event.getApi()))) {
+            if (event.getChannel().getIdAsString() == null ? GuildUtil.getInboxChannelId(guild.getId(), event.getApi()) == null : event.getChannel().getIdAsString().equals(GuildUtil.getInboxChannelId(guild.getId(), event.getApi()))) {
                 event.getApi().getThreadPool().getExecutorService().submit(() -> {
-                    if (event.getMessageContent().startsWith(GuildUtil.botPrefix(server.getId()) + "anonreply ") || event.getMessageContent().startsWith(GuildUtil.botPrefix(server.getId()) + "reply ")) {
+                    if (event.getMessageContent().startsWith(GuildUtil.botPrefix(guild.getId()) + "anonreply ") || event.getMessageContent().startsWith(GuildUtil.botPrefix(guild.getId()) + "reply ")) {
 
                         try {
                             // Gets rid of /anonreply or /reply
-                            String message = (event.getMessageContent().startsWith(GuildUtil.botPrefix(server.getId()) + "anonreply ")) ? event.getMessageContent().replace(GuildUtil.botPrefix(server.getId()) + "anonreply ", "") : event.getMessageContent().replace(GuildUtil.botPrefix(server.getId()) + "reply ", "");
+                            String message = (event.getMessageContent().startsWith(GuildUtil.botPrefix(guild.getId()) + "anonreply ")) ? event.getMessageContent().replace(GuildUtil.botPrefix(guild.getId()) + "anonreply ", "") : event.getMessageContent().replace(GuildUtil.botPrefix(guild.getId()) + "reply ", "");
                             while (message.indexOf(" ") == 0) {
                                 // Gets rid of any extra whitespace
                                 message = message.substring(1);
@@ -144,11 +160,11 @@ public class MessageActions implements MessageCreateListener {
                             }
 
                             EmbedBuilder userMessage = new EmbedBuilder()
-                                    .setThumbnail(server.getIcon().get())
-                                    .setTitle("**The " + server.getName() + " staff have sent you a message**").setColor(Color.green)
+                                    .setThumbnail(guild.getIcon().get())
+                                    .setTitle("**The " + guild.getName() + " staff have sent you a message**").setColor(Color.green)
                                     .addField("Message", message, true)
-                                    .addField("Note:", "To reply, revisit the <#" + GuildUtil.getMessageChannelId(server.getId(), event.getApi()) + "> channel or reach out to a staff member directly.", true);
-                            if (event.getMessageContent().startsWith(GuildUtil.botPrefix(server.getId()) + "reply ")) {
+                                    .addField("Note:", "To reply, revisit the <#" + GuildUtil.getMessageChannelId(guild.getId(), event.getApi()) + "> channel or reach out to a staff member directly.", true);
+                            if (event.getMessageContent().startsWith(GuildUtil.botPrefix(guild.getId()) + "reply ")) {
                                 // Adds the newMessage composer if the command is /reply, and sets the thumbnail to their profile picture
                                 userMessage.setDescription("*Composed by " + event.getMessageAuthor().asUser().get().getNicknameMentionTag() + "*")
                                         .setThumbnail(event.getMessageAuthor().getAvatar());
@@ -172,11 +188,7 @@ public class MessageActions implements MessageCreateListener {
                             // Adds Sent by and Sent to info for the Staff to see
                             event.getChannel().sendMessage("Sent by <@!" + event.getMessageAuthor().getIdAsString() + "> • Sent to <@!" + targetUser.getIdAsString() + ">");
 
-                        } catch (CompletionException | NoSuchElementException ex) {
-                            Mailbox.logger.error(ex.getMessage());
-                        } catch (GuildUtil.InvalidUsageException ex) {
-                            Mailbox.logger.error(ex.getMessage());
-                        } catch (NullPointerException ex) {
+                        } catch (CompletionException | NoSuchElementException | GuildUtil.InvalidUsageException | NullPointerException ex) {
                             Mailbox.logger.error(ex.getMessage());
                         }
 
@@ -189,7 +201,7 @@ public class MessageActions implements MessageCreateListener {
             Mailbox.logger.error(ex.getMessage());
         }
         // verifies that the message starts with the guild prefix
-        if (event.getMessageContent().startsWith(GuildUtil.botPrefix(server.getId()))) {
+        if (event.getMessageContent().startsWith(GuildUtil.botPrefix(guild.getId()))) {
 
             String command = event.getMessageContent().substring(1);
             String[] commandArgs = separator.split(command, 2);
@@ -200,18 +212,18 @@ public class MessageActions implements MessageCreateListener {
 
                     // Check if the author is an administrator
                     if (!event.getMessageAuthor().isServerAdmin()) {
-                        event.getChannel().sendMessage("This command can only be used by a server administrator");
+                        event.getChannel().sendMessage("This command can only be used by a guild administrator");
                         return;
                     }
 
                     if (event.getMessageAuthor().isServerAdmin()) {
-                        String prefix = GuildUtil.botPrefix(server.getId());
+                        String prefix = GuildUtil.botPrefix(guild.getId());
                         EmbedBuilder modCommands = new EmbedBuilder()
                                 .setThumbnail(event.getApi().getYourself().getAvatar())
                                 .setTitle("**Using Mailbox Commands:**").setColor(Color.white)
                                 .addField(prefix + "reply <UserID> <Message>", " Used to reply to users using the Message System module. Can either mention the user or just use their ID" + "\n", true)
                                 .addField(prefix + "anonreply <UserID> <Message>", " Same as the **reply** command, but does not inform the user who sent the message" + "\n", true)
-                                .addField(prefix + "serverinfo", "Provides general information about the server such as owner and user count" + "\n", true)
+                                .addField(prefix + "serverinfo", "Provides general information about the guild such as owner and user count" + "\n", true)
                                 .addField(prefix + "reset", "Resets the entire message system module and deletes channels." + "\n", true)
                                 .addField(prefix + "setup", "Sets up the message system module again, best if used after a reset. (will be ignored if already set up)" + "\n", true)
                                 .addField(prefix + "prefix", "Changes the prefix used by the bot for commands", true);
@@ -225,16 +237,16 @@ public class MessageActions implements MessageCreateListener {
                     event.getApi().getThreadPool().getExecutorService().submit(() -> {
                         // Check if the author is an administrator
                         if (!event.getMessageAuthor().isServerAdmin()) {
-                            event.getChannel().sendMessage("This command can only be used by a server administrator");
+                            event.getChannel().sendMessage("This command can only be used by a guild administrator");
                             return;
                         }
                         try {
-                            GuildUtil.setPrefix(server.getId(), commandArgs[1]);
-                            event.getChannel().sendMessage("The server prefix has been changed to `" + GuildUtil.botPrefix(server.getId()) + "`");
+                            GuildUtil.setPrefix(guild.getId(), commandArgs[1]);
+                            event.getChannel().sendMessage("The guild prefix has been changed to `" + GuildUtil.botPrefix(guild.getId()) + "`");
 
                         } catch (ConfigurationException | FileAlreadyExistsException ex) {
                             Logger.getLogger(MessageActions.class.getName()).log(Level.SEVERE, null, ex);
-                            event.getChannel().sendMessage("The configuration file for this server has not been set up or is corrupted");
+                            event.getChannel().sendMessage("The configuration file for this guild has not been set up or is corrupted");
                         }
                     });
 
@@ -244,20 +256,20 @@ public class MessageActions implements MessageCreateListener {
 
 
                     if (event.getMessageAuthor().isServerAdmin()) {
-                        Date serverCreationDate = Date.from(server.getCreationTimestamp());
+                        Date serverCreationDate = Date.from(guild.getCreationTimestamp());
 
                         EmbedBuilder serverinfo = new EmbedBuilder()
-                                .setThumbnail(server.getIcon().get())
+                                .setThumbnail(guild.getIcon().get())
                                 .setTitle("**Server Information:**").setColor(Color.white)
-                                .addField("**Server Name:**", server.getName(), true)
-                                .addField("**Server ID:**", server.getIdAsString(), true)
-                                .addField("**Member Count:**", Integer.toString(server.getMemberCount()), true)
+                                .addField("**Server Name:**", guild.getName(), true)
+                                .addField("**Server ID:**", guild.getIdAsString(), true)
+                                .addField("**Member Count:**", Integer.toString(guild.getMemberCount()), true)
                                 .addField("**ServerCreation Date:**", serverCreationDate.toString(), true)
-                                .addField("**Server Owner:**", server.getOwner().toString(), true)
-                                .addField("**Server Region:**", server.getRegion().toString(), true)
-                                .addField("**Verification Level:**", server.getVerificationLevel().toString(), true)
-                                .addField("**Text Channels:**", Integer.toString(server.getTextChannels().size()), true)
-                                .addField("**Voice Channels:**", Integer.toString(server.getVoiceChannels().size()), true);
+                                .addField("**Server Owner:**", guild.getOwner().toString(), true)
+                                .addField("**Server Region:**", guild.getRegion().toString(), true)
+                                .addField("**Verification Level:**", guild.getVerificationLevel().toString(), true)
+                                .addField("**Text Channels:**", Integer.toString(guild.getTextChannels().size()), true)
+                                .addField("**Voice Channels:**", Integer.toString(guild.getVoiceChannels().size()), true);
                         event.getChannel().sendMessage(serverinfo);
                     }
                     break;
@@ -265,7 +277,7 @@ public class MessageActions implements MessageCreateListener {
                 case "reset":
 
                     event.getServerTextChannel().ifPresent(serverTextChannel -> {
-                        GuildUtil.resetMessageSystem(server.getId(), event.getServerTextChannel().get(), event.getApi());
+                        GuildUtil.resetMessageSystem(guild.getId(), event.getServerTextChannel().get(), event.getApi());
                     });
 
                     break;
@@ -275,16 +287,15 @@ public class MessageActions implements MessageCreateListener {
 
                     // Check if the author is an administrator
                     if (!event.getMessageAuthor().isServerAdmin()) {
-                        event.getChannel().sendMessage("This command can only be used by a server administrator");
+                        event.getChannel().sendMessage("This command can only be used by a guild administrator");
                         return;
                     }
 
                     event.getApi().getThreadPool().getExecutorService().submit(() -> {
-                        addMessageChannels(server, event.getApi());
+                        addMessageChannels(guild, event.getApi());
                     });
 
                     break;
-
 
             }
 
